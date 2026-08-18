9ab4fd98ffdad2dfe27252c0ff8d5320f2094954
kurmetov <kurmetov@outlook.com>
2026-08-18 16:41:40 +0500

    forgot 1 mid

---
+++ medium/letter_combinations_17.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+
+
+        res = [""]
+        
+        for digit in digits:
+            newres = []
+
+            for comb in res:
+                for letter in phone[digit]:
+                    newres.append(comb + letter)
+
+            res = newres
+
+    
+        return res
